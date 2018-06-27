18653e4aa141a332e9bfa889153e43cc9363754c
mcauliffek4298 <kathryn@peerlift.org>
2018-06-27 16:30:18 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,10 @@
-class Model
-  # Replace with Dog clas
-end
+def get_fortune
+  
+  fortune_array=["You will drink 20 bottles of core water this week", "You will win the next vegetabe off", "Your lunch today will be  delicious", "you will eat bok choy in the near future","You will get a nosebleed on the metro", "You will come across a field of bok choy"]
+  return fortune_array.sample
+  end
+  
+  #puts get_fortune
+  
+  
+  
